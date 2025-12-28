89fbd1a951b21a5807187eecd07cef547122b27a
greta <greta.belegu@gmail.com>
2025-12-27 23:03:03 -0500

    Initial commit: ouat Shopify theme

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  prefix: 'twcss-',
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+  ],
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '750px',
+      lg: '990px',
+      xlg: '1440px',
+      x2lg: '1920px',
+      pageMaxWidth: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        voltra: ['"Voltra"', 'sans-serif'],
+      },
+      colors: {
+        rose: '#e5aa9c',
+        beginning: '#8b5044',
+        garden: '#6b6d20',
+        blush: '#e5aa9c',
+        pillar: '#4c2f3b',
+        sky: '#e2f6fe',
+        cream: '#fff5ee',
+      },
+    },
+  },
+  plugins: [],
+};
